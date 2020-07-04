f2922f3310f51a70ba1a46f3c7a0d4acd583f400
PhillipChong <67701317+PhillipChong@users.noreply.github.com>
2020-07-04 18:52:06 -0400

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -9,18 +9,18 @@ SERVICE_NAME=${NAME}-service
 
 #Store the repositoryUri as a variable
 
-REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'` --debug
+REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} --debug | jq .repositories[].repositoryUri | tr -d'"'`
 
 #Replace the build number and respository URI placeholders with the constants above.
 
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json --debug
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
 
 #Register the task definition in the repository
 
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION} --debug
 
-SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]` --debug
+SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} --debug | jq .failures[]`
 
 #Get latest revision
 
-REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} | jq .taskDefinition.revision` --debug
+REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} --debug | jq .taskDefinition.revision`
