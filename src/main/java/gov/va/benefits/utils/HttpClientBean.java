9ec30b94b34ff2c1558046f203dd70f08437f304
laljith <laljith@yahoo.com>
2020-07-06 14:51:54 -0400

    Adding test cases...

---
+++ src/main/java/gov/va/benefits/utils/HttpClientBean.java
@@ -0,0 +1,20 @@
+package gov.va.benefits.utils;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpClientBean {
+	public <T> T execute(final HttpUriRequest request, final ResponseHandler<? extends T> responseHandler)
+			throws IOException, ClientProtocolException {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		return httpclient.execute(request, responseHandler);
+	}
+}
